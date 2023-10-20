00acc62a6fe72f4cb9d9230b5a6f0ad79a5cbe53
vvu060 <vvu060@gmail.com>
2023-10-20 20:25:43 +0530

    add tags and question in the card

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import React from 'react';
+import RenderTag from '../shared/RenderTag';
 
 interface QuestionCardProps {
   _id: string;
@@ -25,7 +27,28 @@ const QuestionCard = ({
   answers,
   createdAt,
 }: QuestionCardProps) => {
-  return <div>QuestionCard</div>;
+  return (
+    <div className='card-wrapper rounded-[10px] p-9 sm:px-11'>
+      <div className='flex flex-col-reverse items-start justify-between gap-5 sm:flex-row'>
+        <div className=''>
+          <span className='subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden'>
+            {String(createdAt)}
+          </span>
+          <Link href={`/question/${_id}`}>
+            <h3 className='sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1 overflow-hidden'>
+              {title}
+            </h3>
+          </Link>
+        </div>
+        {/* TODO: If signed in add edit delete actions */}
+      </div>
+      <div className='mt-3.5 flex flex-wrap gap-2'>
+        {tags.map((tag) => (
+          <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionCard;
