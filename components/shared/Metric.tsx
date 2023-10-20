bf15fd6eecd4b401478aba2ac7b17d2b9f7d0239
vvu060 <vvu060@gmail.com>
2023-10-20 20:31:32 +0530

    create metric component with props

---
+++ components/shared/Metric.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: number;
+  title: string;
+  href?: string;
+  textStyles: string;
+  isAuthor?: boolean;
+}
+
+const Metric = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  textStyles,
+  isAuthor,
+}: MetricProps) => {
+  return <div>Metric</div>;
+};
+
+export default Metric;
