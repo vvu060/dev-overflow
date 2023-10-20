41048df54f881d5469bdf64cad0309e682d9cb72
vvu060 <vvu060@gmail.com>
2023-10-20 20:43:17 +0530

    add provision handle link to go to user profile

--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 interface MetricProps {
   imgUrl: string;
   alt: string;
-  value: number;
+  value: number | string;
   title: string;
   href?: string;
-  textStyles: string;
+  textStyles?: string;
   isAuthor?: boolean;
 }
 
@@ -19,7 +21,36 @@ const Metric = ({
   textStyles,
   isAuthor,
 }: MetricProps) => {
-  return <div>Metric</div>;
+  const metricContent = (
+    <>
+      <Image
+        src={imgUrl}
+        alt={alt}
+        width={16}
+        height={16}
+        className={`object-contain ${href ? 'rounded-full' : ''}`}
+      />
+      <p className={`${textStyles} flex items-center gap-1`}>
+        {value}
+        <span
+          className={`small-regular line-clamp-1 ${
+            isAuthor ? 'max-sm:hidden' : ''
+          }`}
+        >
+          {title}
+        </span>
+      </p>
+    </>
+  );
+
+  if (href) {
+    return (
+      <Link href={href} className='flex-center gap-1'>
+        {metricContent}
+      </Link>
+    );
+  }
+  return <div className='flex-center flex-wrap gap-1'>{metricContent}</div>;
 };
 
 export default Metric;
