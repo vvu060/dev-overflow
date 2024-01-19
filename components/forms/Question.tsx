a5891b6c272ef610a4408add9859447563eef325
vvu060 <vvu060@gmail.com>
2024-01-19 14:19:22 +0530

    Add TinyMCE editor and update dependencies

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -18,6 +20,7 @@ import { Input } from '@/components/ui/input';
 import { QuestionsSchema } from '@/lib/validations';
 
 const Question = () => {
+  const editorRef = useRef(null);
   // 1. Define your form.
   const form = useForm<z.z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -53,12 +56,88 @@ const Question = () => {
                 <Input
                   placeholder=''
                   {...field}
-                  className='no-focus paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                  className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
                 />
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500'>
-                Bes specific and imagine you&apos;re asking a question to
-                another person
+                Be specific and imagine you&apos;re asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage className='text-red-500' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='explanation'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='paragraph-semibold text-dark400_light800'>
+                Detailed explanation of your problem{' '}
+                <span className='text-primary-500'>*</span>
+              </FormLabel>
+              <FormControl className='mt-3.5'>
+                {/* TODO: Add an editor */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  // @ts-ignore
+                  onInit={(evt, editor) => (editorRef.current = editor)}
+                  initialValue=''
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'print',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
+                    ],
+                    toolbar:
+                      'undo redo | ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter |' +
+                      'alignright alignjustify | bullist numlist',
+                    content_style: 'body { font-family:Inter; font-size:16px }',
+                  }}
+                />
+              </FormControl>
+              <FormDescription className='body-regular mt-2.5 text-light-500'>
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
+              </FormDescription>
+              <FormMessage className='text-red-500' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='tags'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col'>
+              <FormLabel className='paragraph-semibold text-dark400_light800'>
+                Tags <span className='text-primary-500'>*</span>
+              </FormLabel>
+              <FormControl className='mt-3.5'>
+                <Input
+                  placeholder='Add tags...'
+                  {...field}
+                  className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                />
+              </FormControl>
+              <FormDescription className='body-regular mt-2.5 text-light-500'>
+                Add upto 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
               </FormDescription>
               <FormMessage className='text-red-500' />
             </FormItem>
