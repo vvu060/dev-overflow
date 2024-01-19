f09754df051f22947c70e2ca43aa7208ede47976
vvu060 <vvu060@gmail.com>
2024-01-19 13:54:14 +0530

    Add ask-question page component

---
+++ app/(root)/(home)/ask-question/page.tsx
@@ -0,0 +1,14 @@
+import Question from '@/components/forms/Question';
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+      <div className='mt-9'>
+        <Question />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
