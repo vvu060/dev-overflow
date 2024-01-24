1599ab0db96816943afb2f4028f87b215507298a
vvu060 <vvu060@gmail.com>
2024-01-24 12:54:08 +0530

    Add createQuestion function to create a question and update tags

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,9 +1,50 @@
 'use server';
 
+import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
 import { connectToDatabase } from '../mongoose';
 
 export const createQuestion = async (params: any) => {
   try {
     connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    // Create the question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    // Create the tags or get them if they already exist
+    const tagDocuments = [];
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        {
+          name: { $regex: new RegExp(`^${tag}$`, 'i') },
+        },
+        {
+          $setOnInsert: { name: tag },
+          $push: { questions: question._id },
+        },
+        {
+          upsert: true,
+          new: true,
+        }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    // Update the question with the tags
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    // Create an interaction record for the user's ask_question action
+
+    // Increment author's reputation by +5 points for creating a question
   } catch (error) {}
 };
