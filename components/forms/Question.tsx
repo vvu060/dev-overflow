dc65cb787257c8b383c975a0610c5b5bde8d372a
vvu060 <vvu060@gmail.com>
2024-01-24 12:53:51 +0530

    Refactor Question component and add createQuestion functionality

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,12 +21,19 @@ import { QuestionsSchema } from '@/lib/validations';
 import { Badge } from '../ui/badge';
 import Image from 'next/image';
 import { createQuestion } from '@/lib/actions/question.action';
+import { useRouter, usePathname } from 'next/navigation';
 
 const type: any = 'create';
 
-const Question = () => {
+interface QuestionProps {
+  mongoUserId: string;
+}
+
+const Question = ({ mongoUserId }: QuestionProps) => {
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm<z.z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -44,15 +51,22 @@ const Question = () => {
       // make async call to api to create a question
       // contain all form data
 
-      await createQuestion({});
+      await createQuestion({
+        title: values.title,
+        content: values.explanation,
+        tags: values.tags,
+        author: JSON.parse(mongoUserId),
+      });
+
       // navigate to home
+      router.push('/');
     } catch (error) {
     } finally {
       setIsSubmitting(false);
     }
   };
 
-  const handleInputKeyDown = (
+  const handleKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>,
     field: any
   ) => {
@@ -65,7 +79,7 @@ const Question = () => {
         if (tagValue.length > 15) {
           return form.setError('tags', {
             type: 'required',
-            message: 'Tags can be a maximum of 15 characters long.',
+            message: 'Tag must be less than 15 characters',
           });
         }
 
@@ -174,33 +188,41 @@ const Question = () => {
           render={({ field }) => (
             <FormItem className='flex w-full flex-col'>
               <FormLabel className='paragraph-semibold text-dark400_light800'>
-                Tags <span className='text-primary-500'>*</span>
+                Question Title
+                <span className='text-primary-500'>*</span>
               </FormLabel>
               <FormControl className='mt-3.5'>
                 <>
                   <Input
-                    placeholder='Add tags...'
-                    {...field}
-                    className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
-                    onKeyDown={(e) => handleInputKeyDown(e, field)}
-                    onChange={(e) => field.onChange(e.target.value)}
+                    disabled={type === 'Edit'}
+                    className='no-focus paragraph-regular background-light900_dark300
+                light-border-2 text-dark300_light700 min-h-[56px] border'
+                    placeholder='Add Tags...'
+                    onKeyDown={(e) => handleKeyDown(e, field)}
                   />
                   {field.value.length > 0 && (
                     <div className='flex-start mt-2.5 gap-2.5'>
-                      {field?.value.map((tag) => (
+                      {field.value.map((tag) => (
                         <Badge
                           key={tag}
-                          className='subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize'
-                          onClick={() => handleTagRemove(tag, field)}
+                          className='subtle-medium background-light800_dark300 text-light400_light500 flex items-center
+                        justify-center gap-2 rounded-md border-none px-4 py-2 capitalize'
+                          onClick={() =>
+                            type !== 'Edit'
+                              ? handleTagRemove(tag, field)
+                              : () => {}
+                          }
                         >
                           {tag}
-                          <Image
-                            src='/assets/icons/close.svg'
-                            alt='close icon'
-                            width={12}
-                            height={12}
-                            className='cursor-pointer object-contain invert-0 dark:invert'
-                          />
+                          {type !== 'Edit' && (
+                            <Image
+                              src='/assets/icons/close.svg'
+                              alt='Close icon'
+                              width={12}
+                              height={12}
+                              className='cursor-pointer object-contain dark:invert'
+                            />
+                          )}
                         </Badge>
                       ))}
                     </div>
@@ -208,8 +230,8 @@ const Question = () => {
                 </>
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500'>
-                Add upto 3 tags to describe what your question is about. You
-                need to press enter to add a tag.
+                Add up to 5 tags to describe what your question is about. Start
+                typing to see suggestions.
               </FormDescription>
               <FormMessage className='text-red-500' />
             </FormItem>
